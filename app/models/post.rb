2c79faa4e753ef34a476c54f0794c3e6ddaee8dc
stephen <stevedogbey@gmail.com>
2022-10-21 08:45:39 +0000

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction) }
+
+    validate :title_is_clickbait
+
+
+    TITLES = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i ]
+
+  def title_is_clickbait
+    if TITLES.none? { |t| t.match title }
+      errors.add(:title, "Sorry title not present")
+    end
+  end
 end
